8caace626d593499a85ca0bc00e9ad93b6b07707
Tim Barrass <tim.barrass@gmail.com>
2012-10-16 08:01:15 +0100

    attempt to lock file for read, write with retry

--- Stores/FileSystemDataStore.cs
+++ Stores/FileSystemDataStore.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
 using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading;
 
 namespace Stores
 {
@@ -9,39 +11,77 @@ namespace Stores
     {
         public void Write(string name, IEnumerable<T> data)
         {
+            var allowedAttempts = 3;
+            var attempt = 1;
+
             var zipFileName = ZipFileName(name);
 
-            using(var os = new MemoryStream())
-            using (var gzo = new FileStream(zipFileName, FileMode.Create))
-            using (var gz = new GZipStream(gzo, CompressionMode.Compress))
+            using (var os = new MemoryStream())
             {
                 var bf = new BinaryFormatter();
 
                 bf.Serialize(os, data);
 
-                os.Seek(0, SeekOrigin.Begin);
+                while (attempt++ <= allowedAttempts)
+                {
+                    try
+                    {
+                        using (var gzo = new FileStream(zipFileName, FileMode.Create, FileAccess.Write, FileShare.None))
+                        using (var gz = new GZipStream(gzo, CompressionMode.Compress))
+                        {
+                            os.Seek(0, SeekOrigin.Begin);
 
-                os.CopyTo(gz);
+                            os.CopyTo(gz);
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        if (attempt == allowedAttempts) throw;
+
+                        // Default to simple block to avoid complication of checking for cancellation.
+                        // If something more complex is needed, inject a backoff strategy for use here ...
+                        Thread.Sleep(100);
+                    }
+                }
             }
         }
 
         public IEnumerable<T> Read(string name)
         {
+            var allowedAttempts = 3;
+            var attempt = 1;
+
             var zipFileName = ZipFileName(name);
 
-            IEnumerable<T> ret;
+            IEnumerable<T> ret = new List<T>();
 
             using (var gzo = new MemoryStream())
-            using (var gzi = new FileStream(zipFileName, FileMode.Open))
-            using (var gz = new GZipStream(gzi, CompressionMode.Decompress))
             {
-                gz.CopyTo(gzo);
+                while (attempt++ <= allowedAttempts)
+                {
+                    try
+                    {
+                        using (var gzi = new FileStream(zipFileName, FileMode.Open))
+                        using (var gz = new GZipStream(gzi, CompressionMode.Decompress))
+                        {
+                            gz.CopyTo(gzo);
 
-                gzo.Seek(0, SeekOrigin.Begin);
+                            gzo.Seek(0, SeekOrigin.Begin);
+                        }
 
-                var bf = new BinaryFormatter();
+                    }
+                    catch (Exception)
+                    {
+                        if (attempt == allowedAttempts) throw;
+                        
+                        // As for Write -- default to simplest backoff
+                        Thread.Sleep(100);
+                    }
 
-                ret = bf.Deserialize(gzo) as IEnumerable<T>;
+                    var bf = new BinaryFormatter();
+
+                    ret = bf.Deserialize(gzo) as IEnumerable<T>;
+                }
             }
 
             return ret;
--- Writers/SingleWriter.cs
+++ Writers/SingleWriter.cs
@@ -1,11 +1,16 @@
+using System;
+using System.IO;
 using Data;
 using Sinks;
 using Stores;
+using log4net;
 
 namespace Writers
 {
     public class SingleWriter<T> : IDataWriter
     {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(SingleWriter<T>).Name);
+
         private ISnapshotProvider<IMetricData> _snapshotProvider;
 
         private MetricSpecification _spec;
@@ -23,13 +28,20 @@ namespace Writers
             _store = store;
 
             _directory = outputDirectory;
+
+            if (!Directory.Exists(outputDirectory))
+            {
+                Directory.CreateDirectory(outputDirectory);
+
+                Log.Info("Writer for '" + spec.Name + "' created output directory '" + outputDirectory + "'.");
+            } 
         }
 
         public void Write()
         {
             var snapshot = _snapshotProvider.Snapshot(_spec.Name);
 
-            var path = System.IO.Path.Combine(_directory, _spec.Name);
+            var path = Path.Combine(_directory, _spec.Name);
 
             _store.Write(path, snapshot);
         }
