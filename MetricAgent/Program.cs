6ce7b00810880799756fba474ba737572ab49416
Tim Barrass <tim.barrass@gmail.com>
2012-10-18 17:41:31 +0100

    commented code, tsk

--- MetricAgent/Program.cs
+++ MetricAgent/Program.cs
@@ -202,26 +202,4 @@ namespace MetricAgent
 
         private List<IDataSink<IMetricData>> _sinks = new List<IDataSink<IMetricData>>(); 
     }
-
-    //[RunInstaller(true)]
-    //public class MetricAgentInstaller : Installer
-    //{
-    //    private ServiceInstaller serviceInstaller1;
-    //    private ServiceProcessInstaller processInstaller;
-
-    //    public MetricAgentInstaller()
-    //    {
-    //        processInstaller = new ServiceProcessInstaller();
-    //        serviceInstaller1 = new ServiceInstaller();
-
-    //        processInstaller.Account = ServiceAccount.LocalSystem;
-
-    //        serviceInstaller1.StartType = ServiceStartMode.Automatic;
-
-    //        serviceInstaller1.ServiceName = "Metric Agent";
-
-    //        Installers.Add(serviceInstaller1);
-    //        Installers.Add(processInstaller);
-    //    }
-    //}
 }
