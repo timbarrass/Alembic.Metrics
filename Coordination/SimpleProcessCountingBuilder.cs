4a9c3ccf1dfda97315fa3db7390446243cd5ebf9
Tim Barrass <tim.barrass@gmail.com>
2013-04-15 20:16:08 +0100

    tidying some references

--- Coordination/SimpleProcessCountingBuilder.cs
+++ Coordination/SimpleProcessCountingBuilder.cs
@@ -1,8 +1,5 @@
-using System;
 using System.Collections.Generic;
 using Configuration;
-using Data;
-using Sinks;
 using Sources;
 
 namespace Coordination
--- Coordination/SimpleProcessUptimeBuilder.cs
+++ Coordination/SimpleProcessUptimeBuilder.cs
@@ -1,9 +1,5 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using Configuration;
-using Data;
-using Sinks;
 using Sources;
 
 namespace Coordination
