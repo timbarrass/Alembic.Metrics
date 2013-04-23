3b7cc7b90d5007ddb02b3dcef60707f51b6e6e83
Tim Barrass <tim.barrass@gmail.com>
2013-04-23 08:30:10 +0100

    added Big Outer Try
    just after crossing thread boundary to trap unhandled exceptions (e.g.
    in configuration parsing, task startup).

--- App/MetricAgent/Program.cs
+++ App/MetricAgent/Program.cs
@@ -54,52 +54,72 @@ namespace MetricAgent
         {
             Log.Info("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
 
+            _tokenSource = new CancellationTokenSource();
+            _cancellationToken = _tokenSource.Token;
+
             new Thread(Run).Start();
         }
 
         private void Run(object state)
         {
-            var configuration = ConfigurationManager.OpenExeConfiguration("MetricAgent.exe");
-
-            var parser = new ConfigurationParser();
-
-            var schedules = parser.Parse(configuration);
-
-            foreach (var preload in schedules.PreloadSchedules)
-            {
-                preload.RunOnce();
-            }
-
-            _tokenSource = new CancellationTokenSource();
-            _cancellationToken = _tokenSource.Token;
-
-            var tasks = new List<Task>();
-
-            foreach (var schedule in schedules.Schedules)
-            {
-                var currentSchedule = schedule;
-
-                tasks.Add(Task.Factory.StartNew(() => currentSchedule.Start(_cancellationToken), _cancellationToken));
-            }
+            // Other side of thread boundary here, so want to trap all exceptions to avoid
+            // bringing the app down without a chance of acting
 
             try
             {
-                Task.WaitAny(tasks.ToArray());
+                var configuration = ConfigurationManager.OpenExeConfiguration("MetricAgent.exe");
+
+                var parser = new ConfigurationParser();
+
+                var schedules = parser.Parse(configuration);
+
+                foreach (var preload in schedules.PreloadSchedules)
+                {
+                    preload.RunOnce();
+                }
+
+                var tasks = new List<Task>();
+
+                foreach (var schedule in schedules.Schedules)
+                {
+                    var currentSchedule = schedule;
+
+                    tasks.Add(Task.Factory.StartNew(() => currentSchedule.Start(_cancellationToken), _cancellationToken));
+                }
+
+                try
+                {
+                    Task.WaitAny(tasks.ToArray());
+                }
+                catch (AggregateException ae)
+                {
+                    ae.Handle(x =>
+                        {
+                            Log.Error(ae.Message);
+
+                            return false;
+                        });
+                }
             }
-            catch (AggregateException ae)
+            catch (Exception e)
             {
-                ae.Handle(x =>
-                    {
-                        Log.Error(ae.Message);
-                        return false;
-                    });
+                Log.Error(string.Format("{0}\n{1}", e.Message, e.StackTrace));
+
+                Console.WriteLine("Failed while running MetricAgent, see log for details");
+
+                CancelTasks();
             }
+        }
+
+        private void CancelTasks()
+        {
+            if (_tokenSource != null)
+                _tokenSource.Cancel();            
         }
 
         protected override void OnStop()
         {
-            if(_tokenSource != null)
-                _tokenSource.Cancel();
+            CancelTasks();
 
             Log.Info("========================================================================");
         }
